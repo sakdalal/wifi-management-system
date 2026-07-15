f642c1393239802cbb9402331852a10b5c272781
sakdalal <sakshi19dalal@gmail.com>
2026-07-15 23:15:03 +0530

    applied securityCheck to company registration

--- wifi/src/main/java/com/sak/wifi/service/CompanyService.java
+++ wifi/src/main/java/com/sak/wifi/service/CompanyService.java
@@ -1,6 +1,7 @@
 package com.sak.wifi.service;
 
 import com.sak.wifi.entity.Company;
+import com.sak.wifi.exception.ResourceAlreadyExistsException;
 import com.sak.wifi.repository.CompanyRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -14,6 +15,11 @@ public class CompanyService {
     private final CompanyRepository companyRepository;
 
     public Company saveCompany(Company company){
+        if(companyRepository.existsByEmail(company.getEmail())){
+            throw new ResourceAlreadyExistsException(
+                    "Email Already Exists"
+            );
+        }
         return companyRepository.save(company);
     }
 
