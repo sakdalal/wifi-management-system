a8fe1eaeffa42cd7ade910cbf9efabc5af729cbb
sakdalal <sakshi19dalal@gmail.com>
2026-08-12 22:35:33 +0530

    feat: add dashboard KPI cards,chart and API Integration(react--wifi-management-system-frontend)

--- wifi/src/main/java/com/sak/wifi/Controller/DashboardController.java
+++ wifi/src/main/java/com/sak/wifi/Controller/DashboardController.java
@@ -4,6 +4,7 @@ import com.sak.wifi.dto.DashboardResponseDTO;
 import com.sak.wifi.service.DashboardService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/dashboard")
+@CrossOrigin(origins = "http://localhost:5173")
 public class DashboardController {
 
     private final DashboardService dashboardService;
