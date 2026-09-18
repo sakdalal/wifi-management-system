d45a95f53aa67557426262ae2281f0cbe61e60fb
sakdalal <sakshi19dalal@gmail.com>
2026-09-18 21:21:49 +0530

    feat(auth): implement changes to refresh tokens and company-aware login

---
+++ wifi/src/main/java/com/sak/wifi/dto/ChangePasswordRequest.java
@@ -0,0 +1,22 @@
+package com.sak.wifi.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ChangePasswordRequest {
+
+    @NotBlank(message = "Current password is required")
+    private String currentPassword;
+
+    @NotBlank(message = "New Password is required")
+    @Size(min = 6, message = "New password must be at least 6 characters")
+    private  String newPassword;
+
+    @NotBlank(message = "Confirm Password is required")
+    private String confirmPassword;
+
+}
